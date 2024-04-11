3f8502de0e56e7e798e98ea224b190095038edbf
= <=>
2024-04-11 16:08:49 -0300

    Se corrige bug en admiin de recursos

--- app/admin/resources.rb
+++ app/admin/resources.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Resource do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :title, :description, :link, :summary
+  permit_params :title, :description, :link, :summary, subject_ids:[]
   #
   # or
   #
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,6 +1,6 @@
 class Resource < ApplicationRecord
     def self.ransackable_attributes(auth_object = nil)
-        ["created_at", "description", "id", "id_value", "link", "title", "updated_at"]
+        ["created_at", "description", "id", "id_value", "link", "title", "updated_at", "summary"]
     end
     has_many :subject_relations
     has_many :subjects, through: :subject_relations

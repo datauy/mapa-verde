3188d26267f4992fb5f9430ee05fee2ef1c9d731
= <=>
2024-04-26 15:56:42 -0300

    Se corrigen detalles en próximas acttividades, completes #4

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,9 +1,8 @@
 module ActivitiesHelper
   def activity_address(activity)
-    logger.info("\nADDRESS\n#{activity.inspect}\n")
     content_tag :div, class: 'activity-address' do
       if activity.address.starts_with?('http')
-        link_to activity.address
+        link_to activity.address, activity.address, target: '_blank', data: {turbo: false}
       else
         content_tag :span, "#{activity.address}#{activity.location.present? ? ', '+activity.location.name : ''}#{activity.state.present? ? ', '+activity.state.name : ''}"
       end

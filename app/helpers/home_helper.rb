a67fb14ba274b66f221dd847dc445233fdf15b6b
= <=>
2024-04-26 15:27:52 -0300

    Se agregan links de redes tarea #3

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,10 +1,9 @@
 module HomeHelper
+  # adds link with icon for activities
   def acts_tags(acts)
     res = ''
     if acts.present?
-      logger.info "\n\nACTS TAGS\n SUBJ#{@subjects.inspect}\n"
       acts.each do |act|
-        logger.info "\n\nACTS TAGS\n #{act.inspect}\n"
         res += content_tag :a, class: "btn act-tag", href: activity_path(act[:id]), target: '_blank', data: { turbo: false } do
           concat @subjects[act[:subject_id]][:icon].present? ? image_tag(@subjects[act[:subject_id]][:icon]) : image_tag('/images/icon_default.svg', class: "icon-default")
           concat content_tag :span, "#{act[:title]}, #{act[:starts].present? ? act[:starts].strftime('%Y-%m-%d') : ''}"
